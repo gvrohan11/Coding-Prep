365794c6123543f0759aa7c9ed2f0b958d118f84
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-05-09 05:02:10 -0500

    Create firstBadVersion.cpp

---
+++ firstBadVersion.cpp
@@ -0,0 +1,15 @@
+// Leet Code
+// O(n)
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        for (int i = n; i >= 1; i--) {
+            if (!isBadVersion(i)) return i + 1;
+        }
+        return 1;
+    }
+};
