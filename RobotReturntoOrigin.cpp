1a54635c6378b748bc63408ee2236b148105e747
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-05-17 18:41:15 -0500

    Create RobotReturntoOrigin.cpp

---
+++ RobotReturntoOrigin.cpp
@@ -0,0 +1,23 @@
+// LeetCode
+// O(n) 
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for (char c : moves) {
+            if (c == 'U') {
+                y++;
+            } else if (c == 'D') {
+                y--;
+            } else if (c == 'L') {
+                x--;
+            } else if (c == 'R') {
+                x++;
+            }
+        }
+        if (x == 0 && y == 0) return true;
+        return false;
+    }
+};
