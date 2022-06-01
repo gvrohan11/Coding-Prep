c75b3c44e54c1a6e29d656244d5cc6f73cc46868
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-06-01 17:20:06 -0500

    Update RunningSumof1dArray.cpp

--- RunningSumof1dArray.cpp
+++ RunningSumof1dArray.cpp
@@ -1,6 +1,15 @@
+// LeetCode
+// O(n)
+
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
+        // O(n)
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+        /* O(n^2) - Too Slow
         vector<int> use_this = nums;
         for (int i = 0; i < nums.size(); i++) {
             int amount = 0;
@@ -10,5 +19,6 @@ public:
             use_this.at(i) = amount;
         }
         return use_this;
+        */
     }
 };
