0f6381e9ffec00d8129789e79181b77552311894
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-05-31 21:45:54 -0500

    Create DeleteNodeinaLinkedList.cpp

---
+++ DeleteNodeinaLinkedList.cpp
@@ -0,0 +1,20 @@
+// LeetCode
+// O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* next_node = node->next;
+        node->val = next_node->val;
+        node->next = next_node->next;
+        delete next_node;
+    }
+};
