2351863173efc4f1736bd99f30e21ea2338614e0
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-06-03 22:07:07 -0500

    Create Pascal'sTriangleII.cpp

---
+++ Pascal'sTriangleII.cpp
@@ -0,0 +1,25 @@
+// LeetCode
+// O(numRows * num elements in each row after 2)
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> pascals_triangle = generate(rowIndex + 1);
+        return pascals_triangle.at(rowIndex);
+    }
+    vector<vector<int>> generate(int numRows) {
+        if (numRows == 1) return {{1}};
+        if (numRows == 2) return {{1}, {1, 1}};
+        vector<vector<int>> to_return = {{1}, {1, 1}};
+        for (int i = 2; i < numRows; i++) {
+            vector<int> prev = to_return.at(i - 1);
+            vector<int> add = {1};
+            for (int j = 1; j < prev.size(); j++) {
+                add.push_back(prev.at(j - 1) + prev.at(j));
+            }
+            add.push_back(1);
+            to_return.push_back(add);
+        }
+        return to_return;
+    }
+};
