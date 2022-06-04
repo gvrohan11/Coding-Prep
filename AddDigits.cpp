d887c861a163a3ce605cc821e9e768fcbadbb55a
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-06-03 23:27:58 -0500

    Create AddDigits.cpp

---
+++ AddDigits.cpp
@@ -0,0 +1,11 @@
+// LeetCode
+// O(1)
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num < 10) return num;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+};
