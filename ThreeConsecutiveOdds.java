8e36d7567be27c5c69c3e2d30aec47983ca7bebf
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2022-06-08 17:38:22 -0500

    Create ThreeConsecutiveOdds.java

---
+++ ThreeConsecutiveOdds.java
@@ -0,0 +1,13 @@
+// LeetCode
+// O(n)
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        if (arr.length <= 2) return false;
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0)
+                return true;
+        }
+        return false;
+    }
+}
